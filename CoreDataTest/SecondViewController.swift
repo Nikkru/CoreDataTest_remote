6bd3f15c24f447cb97c53e16b56ac4fc3a2684ea
NikNik <nickkru@gmail.com>
2022-10-09 18:57:20 +0300

    add second VC

---
+++ CoreDataTest/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  CoreDataTest
+//
+//  Created by Nikkru on 09.10.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.7843137255, blue: 0.4274509804, alpha: 1)
+    }
+}
--- CoreDataTest/ViewController.swift
+++ CoreDataTest/ViewController.swift
@@ -9,12 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let contactButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("GoTo", for: .normal)
+        button.setTitleColor(.systemYellow, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-        title = "Home"
+        setupViews()
+        setConsraints()
+        contactButton.addTarget(
+            self,
+            action: #selector(contactButtonTapped),
+            for: .touchUpInside)
+    }
+    
+    @objc private func contactButtonTapped() {
+        let controller = SecondViewController()
+        navigationController?.pushViewController(controller, animated: true)
     }
 
-
+    private func setupViews() {
+        view.backgroundColor = .systemBlue
+        title = "Core Data"
+        view.addSubview(contactButton)
+    }
+    
+    private func setConsraints() {
+        NSLayoutConstraint.activate([
+            contactButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            contactButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            contactButton.heightAnchor.constraint(equalToConstant: 50),
+            contactButton.widthAnchor.constraint(equalToConstant: 150)
+        ])
+    }
 }
 
